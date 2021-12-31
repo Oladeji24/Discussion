5f5e489fa1b932b354541ba549b54f5b9a63daa4
Oladeji24 <51147155+Oladeji24@users.noreply.github.com>
2021-12-31 17:03:37 +0100

    Update

--- Discussion/boards/views.py
+++ Discussion/boards/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-
-    return HttpResponse("Working on a Discussion")
+    return HttpResponse('Hello, World!')
 
 # Create your views here.
